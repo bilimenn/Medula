83b295e3671ad920c4a01b4854e4b108e2734bee
gwendal <gwendal@bilimenn.org>
2016-04-15 17:05:43 +0200

    main documentation file file

---
+++ Prj/Inc/Medula.h
@@ -0,0 +1,21 @@
+/*
+ * Medula.h
+ *
+ *  Created on: 15 avr. 2016
+ *      Author: luten
+ */
+
+#ifndef MEDULA_H_
+#define MEDULA_H_
+
+/*! \page main
+ * 		\ref Ebb
+ * 		\ref bg
+ *  \section Ebb Medula board
+ *  \subsection bg block diagram
+ *  \subsection uc Microcontroler
+ *  \subsection cpu Cpu board
+ */
+
+
+#endif /* MEDULA_H_ */
