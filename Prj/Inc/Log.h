1d1b102a704076b6c84d14f287a6f53b3fb42073
gwendal <gwendal@bilimenn.org>
2016-04-16 13:28:30 +0200

    remove ; in macros

--- Prj/Inc/Log.h
+++ Prj/Inc/Log.h
@@ -13,10 +13,10 @@
 #error "MODULE_NAME must be define"
 #endif
 
-#define LOG_APP(...) Trace_log(MODULE_NAME,TRACE_LOG_APP,__FUNCTION__,__LINE__, __VA_ARGS__);
-#define LOG_ERR(...) Trace_log(MODULE_NAME,TRACE_LOG_ERR,__FUNCTION__,__LINE__, __VA_ARGS__);
-#define LOG_WRN(...) Trace_log(MODULE_NAME,TRACE_LOG_WNG,__FUNCTION__,__LINE__, __VA_ARGS__);
-#define LOG_DBG(...) Trace_log(MODULE_NAME,TRACE_LOG_DBG,__FUNCTION__,__LINE__, __VA_ARGS__);
-#define LOG_INF(...) Trace_log(MODULE_NAME,TRACE_LOG_INF,__FUNCTION__,__LINE__, __VA_ARGS__);
+#define LOG_APP(...) Trace_log(MODULE_NAME,TRACE_LOG_APP,__FUNCTION__,__LINE__, __VA_ARGS__)
+#define LOG_ERR(...) Trace_log(MODULE_NAME,TRACE_LOG_ERR,__FUNCTION__,__LINE__, __VA_ARGS__)
+#define LOG_WRN(...) Trace_log(MODULE_NAME,TRACE_LOG_WNG,__FUNCTION__,__LINE__, __VA_ARGS__)
+#define LOG_DBG(...) Trace_log(MODULE_NAME,TRACE_LOG_DBG,__FUNCTION__,__LINE__, __VA_ARGS__)
+#define LOG_INF(...) Trace_log(MODULE_NAME,TRACE_LOG_INF,__FUNCTION__,__LINE__, __VA_ARGS__)
 
 #endif /* LOG_H_ */
