1b652b555c31a3eafe942b10fc07954d190cd9fe
gwendal <gwendal@bilimenn.org>
2016-04-17 15:38:14 +0200

    add firmware config file

---
+++ Prj/Inc/Medula_conf.h
@@ -0,0 +1,34 @@
+/*
+ * Medula_conf.h
+ *
+ *  Created on: 17 avr. 2016
+ *      Author: luten
+ ******************************************************************************
+    Copyright (C) 2016  Gwendal Le Gall
+    This file is part of Medula Firmware.
+
+    Medula Firmware is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+
+  */
+
+#ifndef MEDULA_CONF_H_
+#define MEDULA_CONF_H_
+
+#define USE_DYNAMIXEL
+//#define USE_ECHO
+//#define USE_IMU
+
+
+#endif /* MEDULA_CONF_H_ */
