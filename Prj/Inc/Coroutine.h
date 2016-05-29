da52ec7472bc2b89b0b805c92ac97c3fcf360050
gwendal <gwendal@bilimenn.org>
2016-05-29 17:05:07 +0200

    update coroutine macros

--- Prj/Inc/Coroutine.h
+++ Prj/Inc/Coroutine.h
@@ -8,27 +8,36 @@
 #ifndef COROUTINE_H_
 #define COROUTINE_H_
 
+typedef struct
+{
+	int iState;
+	int iDelayInit;
+	int iTimeoutInit;
+} Task_Info_t;
 
-#define CR_TASK_INIT()       static int iTaskDelayInit=0,iTimeoutInit;(void)iTaskDelayInit;(void)iTimeoutInit; switch( *piTaskState ) { case 0:
-#define CR_TASK_END()        *piTaskState=-1; default: *piTaskState=-1;} return 0;
+#define TASK_INFO_INIT {0,0,0}
+#define TASK_INFO_RESET( task)  task.iState=0,task.iDelayInit=0,task.iTimeoutInit=0
+
+#define CR_TASK_INIT()       switch( pTask->iState ) { case 0:
+#define CR_TASK_END()        pTask->iState=-1; default: pTask->iState=-1;} return 0;
 #define CR_TASK_YELD()        \
-        *piTaskState = __LINE__; \
+        pTask->iState = __LINE__; \
         return 1;              \
         case __LINE__:
 
 #define CR_TASK_DELAY( nb_ms )                    \
-        iTaskDelayInit= HAL_GetTick();             \
-        *piTaskState = __LINE__;                     \
+         pTask->iDelayInit= HAL_GetTick();             \
+        pTask->iState = __LINE__;                     \
         return 1;                                    \
         case __LINE__:                                 \
-        if((HAL_GetTick()-iTaskDelayInit) < nb_ms) \
+        if((HAL_GetTick()- pTask->iDelayInit) < nb_ms) \
             return 1;
 
 #define CR_TIMEOUT_INIT( value ) \
-		iTimeoutInit= value; \
-		iTaskDelayInit= HAL_GetTick();
+		 pTask->iTimeoutInit= value; \
+		 pTask->iDelayInit= HAL_GetTick();
 
-#define CR_TIMEOUT_TEST()  ((HAL_GetTick() - iTaskDelayInit)>iTimeoutInit)
+#define CR_TIMEOUT_TEST()  ((HAL_GetTick() -  pTask->iDelayInit)> pTask->iTimeoutInit)
 
 #define CR_TASK_EXECUTE(task) while( (task) ) { CR_TASK_YELD();} \
 
